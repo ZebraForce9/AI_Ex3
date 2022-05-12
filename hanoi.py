48fbe0122e7412a750b8d9d3a74448da5e0810c8
adibo <adiborochov1@gmail.com>
2022-05-12 18:35:45 +0300

    did q13

--- hanoi.py
+++ hanoi.py
@@ -5,7 +5,30 @@ def create_domain_file(domain_file_name, n_, m_):
     disks = ['d_%s' % i for i in list(range(n_))]  # [d_0,..., d_(n_ - 1)]
     pegs = ['p_%s' % i for i in list(range(m_))]  # [p_0,..., p_(m_ - 1)]
     domain_file = open(domain_file_name, 'w')  # use domain_file.write(str) to write to domain_file
-    "*** YOUR CODE HERE ***"
+
+    for p in pegs:
+        domain_file.write(f"CLEAR{p}")
+        for d in disks:
+            domain_file.write(f"{d}SMALLER{p}")
+            domain_file.write(f"{d}ON{p}")
+
+    for d in disks:
+        domain_file.write(f"CLEAR{d}")
+
+    for i, d1 in enumerate(disks):
+        for d2 in disks[i + 1:]:
+            domain_file.write(f"{d1}SMALLER{d2}")
+            domain_file.write(f"{d1}ON{d2}")
+
+    for d in disks:
+        for a in disks + pegs:
+            for b in disks + pegs:
+                if (a[0] == "d" and d >= a) or (b[0] == "d" and d >= b):
+                    continue
+                domain_file.write(f"Name: M{d}P{a}P{b}")
+                domain_file.write(f"Pre: CLEAR{d} {d}ON{a} CLEAR{b} {a}SMALLER{b}")
+                domain_file.write(f"Add: CLEAR{a} {d}ON{b}")
+                domain_file.write(f"Del: {d}ON{a} CLEAR{b}")
 
     domain_file.close()
 
